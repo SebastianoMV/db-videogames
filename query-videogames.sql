5d226b68dccadbf47d4730b507592337b5011164
SebastianoMV <sebastianovisco@gmail.com>
2022-09-28 14:14:03 +0200

    Query con group by

--- query-videogames.sql
+++ query-videogames.sql
@@ -72,3 +72,66 @@ where videogame_id = 412;
 select count(id)
 from videogames
 where software_house_id = 1 and YEAR(release_date) = 2018;
+
+
+-- Query con group by
+
+-- 1 Contare quante software house ci sono per ogni paese (3)
+select count(id)
+from software_houses
+group by country;
+
+-- 2 Contare quante recensioni ha ricevuto ogni videogioco (del videogioco vogliamo solo l'ID) (500)
+select count(id)
+from reviews
+group by videogame_id;
+
+-- 3 Contare quanti videogiochi hanno ciascuna classificazione PEGI (della classificazione PEGI vogliamo solo l'ID) (13)
+select COUNT(videogame_id)
+from pegi_label_videogame
+group by pegi_label_id;
+
+-- 4 Mostrare il numero di videogiochi rilasciati ogni anno (11)
+select COUNT(id)
+from videogames
+group by YEAR( release_date);
+
+-- 5 Contare quanti videogiochi sono disponbiili per ciascun device (del device vogliamo solo l'ID) (7)
+select COUNT(videogame_id)
+from device_videogame
+group by device_id;
+
+-- 6 Ordinare i videogame in base alla media delle recensioni (del videogioco vogliamo solo l'ID) (500)
+select AVG(rating)
+from reviews
+group by videogame_id;
+
+-- Query con join
+
+-- 1 Selezionare i dati di tutti giocatori che hanno scritto almeno una recensione, mostrandoli una sola volta (996)
+
+-- 2 Sezionare tutti i videogame dei tornei tenuti nel 2016, mostrandoli una sola volta (226)
+
+-- 3 Mostrare le categorie di ogni videogioco (1718)
+
+-- 4 Selezionare i dati di tutte le software house che hanno rilasciato almeno un gioco dopo il 2020, mostrandoli una sola volta (6)
+
+-- 5 Selezionare i premi ricevuti da ogni software house per i videogiochi che ha prodotto (55)
+
+-- 6 Selezionare categorie e classificazioni PEGI dei videogiochi che hanno ricevuto recensioni da 4 e 5 stelle, mostrandole una sola volta (3363)
+
+-- 7 Selezionare quali giochi erano presenti nei tornei nei quali hanno partecipato i giocatori il cui nome inizia per 'S' (474)
+
+-- 8 Selezionare le città in cui è stato giocato il gioco dell'anno del 2018 (36)
+
+-- 9 Selezionare i giocatori che hanno giocato al gioco più atteso del 2018 in un torneo del 2019 (3306)
+
+-- *********** BONUS ***********
+
+-- 10 Selezionare i dati della prima software house che ha rilasciato un gioco, assieme ai dati del gioco stesso (software house id : 5)
+
+-- 11 Selezionare i dati del videogame (id, name, release_date, totale recensioni) con più recensioni (videogame id : 398)
+
+-- 12 Selezionare la software house che ha vinto più premi tra il 2015 e il 2016 (software house id : 1)
+
+-- 13 Selezionare le categorie dei videogame i quali hanno una media recensioni inferiore a 1.5 (10)
